e6aa6a7142375aa93c060fbfbb236b085803f83e
EllisCodes-rgb <sutherlandellis9@gmail.com>
2025-08-26 15:44:05 +0100

    tested line breaks and image add

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <div>Hello, world</div>
-  );
+    <main>
+      <h1>Hello, world</h1>
+      <p>This is my learning space</p>
+      <p>seeing visually the difference between line break text <br></br><br></br>and non line break text</p>
+      <p>to better understand how this works</p>
+      <img src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg"/>
+<p>here is a cat</p>
+<a href="https://www.w3schools.com/">My first test link</a>
+</main>
+  )
 }
