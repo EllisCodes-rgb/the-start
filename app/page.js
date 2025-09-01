b1a35505eb00eaa422b8f6e78170b7d74904b770
EllisCodes-rgb <sutherlandellis9@gmail.com>
2025-09-01 19:07:42 +0100

    added styles to text, changing sizes, colours, fonts and backgrounds

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello, world</h1>
-      <p>This is my learning space</p>
+  <main style={{backgroundColor: "grey"}}>
+      <h1 style={{ fontSize: 100, fontFamily: "comicsans", textAlign:"center"}}>Hello, world</h1>
+      <h2 style={{ fontSize: 65, color: "green"}}>Heading 2, less important than heading 1</h2>
+      <p style={{backgroundColor: "purple"}}>This is my learning space</p>
       <p>seeing visually the difference between line break text <br></br><br></br>and non line break text</p>
       <p>to better understand how this works</p>
-      <img src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg"/>
-<p>here is a cat</p>
-<a href="https://www.w3schools.com/">My first test link</a>
+      <img src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg" width="300" height="400" />
+      <p style={{ color: "blue"}}>here is a cat</p>
+      <a href="https://www.w3schools.com/">My first test link</a>
+
 </main>
   )
 }
