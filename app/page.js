905ff3b0998e5cd4877fba1c66855e560dc05b95
EllisCodes-rgb <sutherlandellis9@gmail.com>
2025-09-02 23:43:56 +0100

    added formatting to a few elements

--- app/page.js
+++ app/page.js
@@ -9,7 +9,11 @@ export default function Home() {
       <img src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg" width="300" height="400" />
       <p style={{ color: "blue"}}>here is a cat</p>
       <a href="https://www.w3schools.com/">My first test link</a>
-
+<br></br> <b>the quick brown fox jumps over the lazy dog</b> <br></br>
+<i>the quick brown fox jumps over the lazy dog</i>
+<p> the <del>slow</del> quick brown fox jumps over the <del>scary</del> lazy dog</p>
+<br></br>
+<p>my favourite colour is <em>navy</em></p>
 </main>
   )
 }
