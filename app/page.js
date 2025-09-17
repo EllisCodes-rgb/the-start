41e57e2349a3d0284432dff9012eee22d29c3e76
EllisCodes-rgb <sutherlandellis9@gmail.com>
2025-09-17 16:54:16 +0100

    text and image aligment

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-  <main style={{backgroundColor: "grey"}}>
-      <h1 style={{ fontSize: 100, fontFamily: "comicsans", textAlign:"center"}}>Hello, world</h1>
+  <main style={{backgroundColor: "grey", textAlign: "center"}}>
+      <h1 style={{ fontSize: 100, fontFamily: "comicsans"}}>Hello, world</h1>
       <h2 style={{ fontSize: 65, color: "green"}}>Heading 2, less important than heading 1</h2>
       <p style={{backgroundColor: "purple"}}>This is my learning space</p>
       <p>seeing visually the difference between line break text <br></br><br></br>and non line break text</p>
       <p>to better understand how this works</p>
-      <img src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg" width="300" height="400" />
+      <img style={{display: "inline-block"}} src="https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg" width="300" height="400"/>
       <p style={{ color: "blue"}}>here is a cat</p>
       <a href="https://www.w3schools.com/">My first test link</a>
 <br></br> <b>the quick brown fox jumps over the lazy dog</b> <br></br>
